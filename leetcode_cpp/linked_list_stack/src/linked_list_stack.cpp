c8e3428d0549a2bd465eb2959de41d6ea474e274
hnb <hnbandomachi@gmail.com>
2022-07-14 22:49:37 +0700

    [data_stucture] complete stack

--- leetcode_cpp/linked_list_stack/src/linked_list_stack.cpp
+++ leetcode_cpp/linked_list_stack/src/linked_list_stack.cpp
@@ -1,57 +1,76 @@
 #include <iostream>
+template <typename T>
+class Node {
 
-// class Stack : public Node<int> 
+private:
+	T value;
+	Node* next;
+
+public:
+	Node()
+	{
+		next = NULL;
+	}
+	void set_value(T value)
+	{
+		this->value = value;
+	}
+	void set_next(Node* next)
+	{
+		this->next = next;
+	}
+	T get_value()
+	{
+		return this->value;
+	}
+	Node* get_next()
+	{
+		return this->next;
+	}
+};
+
 template <typename T>
 class Stack
 {
 
 private:
-	class Node {
-
-	private:
-		T value;
-		Node* next;
-
-	public:
-		Node()
-		{
-			next = NULL;
-		}
-		void set_value(T value)
-		{
-			this->value = value;
-		}
-		void set_next(Node* next)
-		{
-			this->next = next;
-		}
-		T get_value()
-		{
-			return this->value;
-		}
-		Node* get_next()
-		{
-			return this->next;
-		}
-	};
+	Node<T>* head;
+	Node<T>* temp;
 
 public:
-	Node* head;
-	Node* current;
 	Stack()
 	{
-		head = new Node;
-		current = head;
+		head	= NULL;
+		temp	= NULL;
 	}
 	void push(T element)
 	{
-		current->set_next(new Node);
-		current->set_value(element);
-		current = current->get_next();
+		if (head == NULL)
+		{
+			head = new Node<T>;
+			head->set_next(NULL);
+		}
+		else
+		{
+			temp = new Node<T>;
+			temp->set_next(head);
+			head = temp;
+		}
+		head->set_value(element);
 	}
 	void pop()
 	{
-
+		if (!is_empty())
+		{
+			temp = head;
+			head = head->get_next();
+			delete temp;
+			temp = NULL;
+		}
+		else
+		{
+			std::cout << "The stack is empty." << std::endl;
+		}
 	}
 	unsigned int size()
 	{
@@ -60,35 +79,35 @@ public:
 	}
 	T top()
 	{
-		T ret;
+		if (!is_empty())
+		{
+			return head->get_value();
+		}
+		else
+		{
+			std::cout << "The stack is empty." << std::endl;
+			return 0;
+		}
 
-		return ret;
 	}
 	bool is_empty()
 	{
-		try
+		if (head == NULL)
 		{
-
+			return true;
 		}
-		catch (T* e)
+		else
 		{
-
-		}
-	}
-	bool is_full()
-	{
-		try
-		{
-
-		}
-		catch (T* e)
-		{
-
+			return false;
 		}
 	}
+
 	void delete_stack()
 	{
-
+		while (head != NULL)
+		{
+			pop();
+		}
 	}
 };
 
@@ -97,7 +116,9 @@ int main(int argc, char** argv)
 {
 	Stack<int>* stack1 = new Stack<int>;
 	stack1->push(5);
-	stack1->push(3);
 	stack1->push(4);
+	stack1->push(3);
+	stack1->delete_stack();
+	std::cout << "stack1 after pop = " << stack1->top() << std::endl;
 	return 0;
 }
