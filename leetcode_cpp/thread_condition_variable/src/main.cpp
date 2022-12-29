d5e04b44634baba481c8d2e2a9a8a5f57b271ccc
huyleincepit <huy.le@incepit.com>
2022-12-29 16:00:14 +0700

    study threadsafe_stack

--- leetcode_cpp/thread_condition_variable/src/main.cpp
+++ leetcode_cpp/thread_condition_variable/src/main.cpp
@@ -9,16 +9,18 @@
 
 std::condition_variable data_ready_event;
 std::mutex data_mutex;
-std::queue<int> data_queue;
+std::queue<unsigned int> data_queue;
 
 
 void prepare_data_thread() {
-    for (int i = 0; i < 10; i++) {
+    unsigned int i = 0;
+    while (true) {
         //std::lock_guard<std::mutex> lk(data_mutex);
         data_mutex.lock();
         data_queue.push(i);
         data_mutex.unlock();
-        std::cout << "\npushed " << i << " into data_queue \n";
+        std::cout << "\nThread ID[" << std::this_thread::get_id() << "] pushed " << i << " into data_queue \n";
+        i = ++i%9;
         data_ready_event.notify_all();
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
@@ -29,7 +31,7 @@ void process_data_thread() {
     while (true) {
         std::unique_lock<std::mutex> lk(data_mutex);
         data_ready_event.wait(lk, []{return !data_queue.empty();});
-        std::cout << "processing data i = " << data_queue.front() << std::endl;
+        std::cout << "Thread ID[" << std::this_thread::get_id() << "] processing data i = " << data_queue.front() << std::endl;
         data_queue.pop();
         lk.unlock();
     }
@@ -42,6 +44,6 @@ int main()
     std::thread process_worker(process_data_thread);
     prepare_woker.detach();
     process_worker.detach();
-    std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::cin.get();
     return 0;
 }
---
+++ leetcode_cpp/threadsafe_stack/src/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <memory>
+#include <stack>
+#include <exception>
+
+struct empty_stack : std::exception {
+	const char* what() const throw();
+};
+
+template<typename T>
+class threadsafe_stack {
+
+private:
+	std::stack<T> _data_stack;
+	mutable std::mutex _mutex;
+
+public:
+	threadsafe_stack() = default;
+	threadsafe_stack(const threadsafe_stack& other) {
+		std::lock_guard<std::mutex> lk(other._mutex);
+		_data_stack = other._data_stack;
+	}
+	threadsafe_stack& operator = (const threadsafe_stack&) = delete;
+
+	void push(T& data) {
+		std::lock_guard<std::mutex> lk(_mutex);
+		_data_stack.push(std::move(data));
+	}
+
+	std::shared_ptr<T> pop() {
+		std::lock_guard<std::mutex> lk(_mutex);
+		if (_data_stack.empty()) {
+			throw empty_stack();
+		}
+		std::shared_ptr<T> const res(std::make_shared<T>(std::move(_data_stack.top()));
+		_data_stack.pop();
+		return res;
+	}
+
+	void pop(T& value) {
+		std::lock_guard<std::mutex> lk(_mutex);
+		if (_data_stack.empty()) {
+			throw empty_stack();
+		}
+		value = std::move(_data_stack.top();
+		_data_stack.pop();
+	}
+
+	bool empty() const {
+		std::lock_guard<std::mutex> lk(_mutex);
+		return _data_stack.empty();
+	}
+};
+
+int main() {
+
+	return 0;
+}
