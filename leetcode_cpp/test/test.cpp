317af73d1b9e9b9b2d2d01e1389cbb9bcd1a48a5
huyle <huy.le@incepit.com>
2023-03-25 09:45:39 +0700

    [huyle][tsme] add can

--- leetcode_cpp/test/test.cpp
+++ leetcode_cpp/test/test.cpp
@@ -3,47 +3,49 @@
 //#define cplusplus_com_reference_atomic_atomic_atomic
 //#define listing_5_4
 //#define listing_9_1
-#define volatile_example
+//#define volatile_example
+//#define function_pointer
+#define factory_method_pattern
 
 #ifdef test1 
 
-#include <iostream>
-#include <string>
-#include <memory>
+#include <cctype>
+#include <sstream>
+#include <iomanip>
 
-class Rectangle {
-private:
-	uint32_t length;
-	uint32_t breadth;
 
-public:
-	Rectangle(uint32_t LENGTH, uint32_t BREADTH) :length(LENGTH), breadth(BREADTH) {}
-	~Rectangle() {}
+bool decode(const char* hexStr, unsigned char* plainData, size_t hexStrLen)
+{
+	if (!hexStrLen)
+		hexStrLen = strlen(hexStr);
 
-	uint32_t area() {
-		return length * breadth;
+	std::stringstream hexStringStream;
+	hexStringStream >> std::hex;
+
+	for (size_t hexId = 0, binId = 0; hexId < hexStrLen; ++binId) {
+
+		const char tmpStr[3] = { hexStr[hexId++], hexStr[hexId++], 0 };
+		if (!isxdigit(tmpStr[0]) || !isxdigit(tmpStr[1]))
+			//return false;
+			int a;
+
+		hexStringStream.clear();
+		hexStringStream.str(tmpStr);
+
+		int tmpValue = 0;
+
+		hexStringStream >> tmpValue;
+		plainData[binId] = static_cast<unsigned char>(tmpValue);
 	}
-};
+
+	return true;
+}
 
 int main() {
-	Rectangle* rec_ptr0 = new Rectangle(1, 2);
-	std::unique_ptr<Rectangle> rec_ptr1 = std::make_unique<Rectangle>(3, 5);
-	std::unique_ptr<Rectangle> rec_ptr2(new Rectangle(5, 6));
-	std::unique_ptr<Rectangle> rec_ptr3(nullptr);
-	rec_ptr3.reset(new Rectangle(6, 7));
-	std::unique_ptr<Rectangle> rec_ptr4(std::move(rec_ptr1));
-	if (rec_ptr1.get()) {
-		std::cout << "Area of rec_ptr1: " << rec_ptr1->area() << std::endl;
-	}
-	std::shared_ptr<Rectangle> rec_ptr5(rec_ptr0);
-	std::shared_ptr<Rectangle> rec_ptr6 = rec_ptr5;
-	std::shared_ptr<Rectangle> rec_ptr7 = rec_ptr5;
-	std::shared_ptr<Rectangle> rec_ptr8 = rec_ptr5;
-
-	std::cout << "use_count() rec_ptr5 = " << rec_ptr5.use_count() << std::endl;
-	std::cout << "use_count() rec_ptr6 = " << rec_ptr6.use_count() << std::endl;
-	std::cout << "use_count() rec_ptr7 = " << rec_ptr7.use_count() << std::endl;
-	std::cout << "use_count() rec_ptr8 = " << rec_ptr8.use_count() << std::endl;
+	const char* hexStr = "lequanghuy123";
+	unsigned char* plaindata = new unsigned char[strlen(hexStr)]();
+	size_t len = 0;
+	decode(hexStr, plaindata, len);
 
 	return 0;
 }
@@ -291,3 +293,138 @@ int main() {
 }
 
 #endif // volatile_example
+
+
+#ifdef function_pointer
+
+#include <iostream>
+
+typedef enum
+{
+	RIPE_EVT_BOX_CONNECTED,	//!< Box connection established. Box is not ready to accept any command yet. Wait for RIPE_EVT_BOX_STATIC_UPDATE to start communicating the box
+	RIPE_EVT_BOX_DISCONNECTED,				//!< Box lost connection, the SDK will attempt to reestablish communication automatically, unless box disconnect request was not issued by the User using RIPEBoxDisconnect() API
+	RIPE_EVT_BOX_STATUS_UPDATE,				//!< Box's dynamic status data available. Use RIPEGetBoxStatusData(..) API to access the data. Period of the event is approximately 2 seconds.
+	RIPE_EVT_BOX_BATTLOW,		  			//!< NOT SUPPORTED! Box's battery charge is less than 10%, fired every 2 sec (approx) till the battery is restored 
+	RIPE_EVT_BOX_TILTED,		  			//!< NOT SUPPORTED! Box's tilted. Fired every 2 sec, till the tilt is physically adjusted. 
+	RIPE_EVT_DATA_TRIGGERDETECTED,			//!< Box detected Trigger/shot, this event will be followed by a series of events such as speed, spin results. Additionally box enters DISARM'ed state and SDK-USER is expected to wait for RIPE_EVT_DATA_SPEEDREADY irrespective of valid or invalid shot.
+	RIPE_EVT_DATA_SPEEDREADY,				//!< Speed Result are available, follows a successful trigger event, user can get access to speed & flight model params using relavant RIPEGet APIS. Box maintains the DISARM'ed state and SDK-USER is expected to wait for RIPE_EVT_DATA_SPINREADY irrespective of valid or invalid shot
+	RIPE_EVT_DATA_SPINREADY,				//!< Spin Result are available, follows a successful speed event, user can get access to spin data & flight model params using relavant RIPEGet APIS. Box maintains DISARM'ed state, which can be released here or later from this point by issuing RIPE_CMD_ARM command. 
+	RIPE_EVT_DATA_USERPERSISTENTDATA_READY,	//!< Custom Data read from Box's non-volatile memory is available, user can get access to data using RIPEBoxGetUserPersistentData(..) API
+	RIPE_EVT_FW_UPGRADE_AVAILABLE,          //!< Firmware Upgrade is available, event triggered only during the initial connection following a cold boot of the Box and APP is expected to start FirmwareUpgrade process issuing RIPE_CMD_UPGRADE_FIRMWARE box command
+	RIPE_EVT_FW_SDK_INCOMPATIBLE,			//!< Upgrade is mandatetory for proper functioning of the Box. Without upgrade any following command/request to box would fail
+	RIPE_EVT_FW_SDK_CUSTOMERCODE_INCOMPATIBLE, //!< SDK & Box's customer code mismatch, any following command/request to box would fail
+	RIPE_EVT_FW_UPGRADE_SUCCESS, 			//!< Firmware Upgrade was successful, box will now try to restart automatically 
+	RIPE_EVT_FW_UPGRADE_ERROR,				//!< Firmware Upgrade failed, upgrade fails due to connection or other data transfer errors, Box will revert back to its current firmware version
+	RIPE_EVT_COMM_DISCOVER_UPDATE,			//!< Ready with list of boxes found (found network+USB adapters). Use RIPEGetDiscoveredBoxList(..) API to access the box lists
+	RIPE_EVT_BOX_STATIC_UPDATE,				//!< Box's static data available. Use RIPEGetBoxStaticData(..) API to access the data. Event is fired only once after Box connection. From this point in time, box is ready to accept command issued using RIPESendBoxCommand(..) API, for example issuing RIPE_CMD_ARM command to prepare the box to take shots.
+	RIPE_EVT_BOX_ERROR,						//!< Box entered ERROR state. Possible reasons, box data corrupted, which is a irrecoverable state.
+	RIPE_EVT_NETWORK_SCAN_LIST_UPDATE,		//!< Ready with list of wifi access points seen by the connected box,  Use RIPEGetNetworkScanList(..) API to access the box lists
+	RIPE_EVT_BIT_RESERVED_2,				//!< RESERVED
+	RIPE_EVT_BIT_RESERVED_3,				//!< RESERVED
+	RIPE_EVT_BIT_RESERVED_4,				//!< RESERVED	
+	RIPE_EVT_LAST							//!< RESERVED
+}RIPEEventType;
+
+void callbackTask(void* HANDLE, RIPEEventType EVT);
+
+struct RIPEInitType {
+	void* handle;
+	void (*callback)(void* handle, RIPEEventType latestEvent);
+	void (*callback2)(void*, RIPEEventType);
+	char dataFolderPath[2048];
+};
+
+
+int main() {
+	RIPEInitType x1;
+	void* hnd1 = (void*) 0x1;
+	void* hnd2 = (void*) 0x2;
+
+	RIPEEventType e1 = RIPE_EVT_BOX_BATTLOW;
+	RIPEEventType e2 = RIPE_EVT_BOX_DISCONNECTED;
+
+	x1.callback = callbackTask;
+	x1.callback2 = callbackTask;
+
+	x1.callback(hnd1, e1);
+	x1.callback2(hnd2, e2);
+
+	return 0;
+}
+
+
+void callbackTask(void* HANDLE, RIPEEventType EVT) {
+	std::cout << "HANLDE \t= " << (int)HANDLE << std::endl;
+	std::cout << "EVT \t= " << EVT << std::endl;
+}
+
+#endif // function_pointer
+
+#ifdef factory_method_pattern
+
+#include <iostream>
+
+class Product {
+public:
+	virtual void use() = 0;
+};
+
+class ProductA : public Product {
+public:
+	void use() override {
+		std::cout << "Using ProductA" << std::endl;
+	}
+};
+
+class ProductB : public Product {
+public:
+	void use() override {
+		std::cout << "Using ProductB" << std::endl;
+	}
+};
+
+class Creator {
+public:
+	virtual Product* create_product() = 0;
+};
+
+class CreatorA : public Creator {
+public:
+	Product* create_product() override {
+		return new ProductA();
+	}
+};
+
+class CreatorB : public Creator {
+public:
+	Product* create_product() override {
+		return new ProductB();
+	}
+};
+
+
+int main() {
+	Creator* creator(nullptr);
+	Product* product(nullptr);
+
+	creator = new CreatorA();
+	product = creator->create_product();
+	product->use();
+
+	delete creator, product;
+	creator = nullptr;
+	product = nullptr;
+
+
+	creator = new CreatorB();
+	product = creator->create_product();
+	product->use();
+	
+	delete creator, product;
+	creator = nullptr;
+	product = nullptr;
+
+	return 0;
+}
+
+#endif // factory_method_pattern
